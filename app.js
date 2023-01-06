e92b363b134a4c6a1aeeb5486b6931431422d02f
Marcus <marcus.urani20@gmail.com>
2023-01-06 12:02:19 -0300

    Alterado o objeto de pessoas

--- app.js
+++ app.js
@@ -5,54 +5,63 @@ const date = new Date();
 
 const people = [
   {
+    id: "1",
     name: "Mario Henrique dos Santos",
-    photo: "man_3",
+    photo: "https://imgur.com/EKdokDm.png",
     comment:
       "Sensacional, finalmente pude me livrar das dívidas e construir um patrimônio que me deixa seguro. 🤩",
   },
   {
+    id: "2",
     name: "Lucas Fernandes",
-    photo: "man_1",
+    photo: "https://imgur.com/8fj2UHl.png",
     comment:
       "Nunca tinha conseguido me livrar das dívidas e quase perdi minha esposa por isso, mais agora finalmente consegui sair do aperto e agradeço muito por isso",
   },
   {
+    id: "3",
     name: "Marta Silva",
-    photo: "woman_1",
+    photo: "https://imgur.com/pjc4M1c.png",
     comment:
       "Depois de anos no aperto e recebendo cobranças todos os dias, finalmente me livrei das dívidas e agora posso dizer q tenho uma vida confortável e sem preocupação",
   },
   {
+    id: "4",
     name: "Marcus Vinícius",
-    photo: "man_2",
+    photo: "https://imgur.com/vlwzs4b.png",
     comment: "Consegui me livrar das dívidas 🥳🥳",
   },
   {
+    id: "5",
     name: "Brunna Nunes",
-    photo: "woman_4",
+    photo: "https://imgur.com/kF1TOl4.png",
     comment:
       "Posso ter uma vida tranquila agora e sem me preocupar com os boletos 😜",
   },
   {
+    id: "6",
     name: "Andressa Fernandes Melo",
-    photo: "woman_3",
+    photo: "https://imgur.com/UHtnJ40.png",
     comment:
       "Tinha muitos problemas com dívidas, mas agora elas não são mais um problema pra mim e posso dormir tranquila",
   },
   {
+    id: "7",
     name: "Luiz Gustavo",
-    photo: "man_4",
+    photo: "https://imgur.com/AC92x8B.png",
     comment:
       "Finalmente um método que REALMENTE FUNCIONA, agora estou livre das dívidas 🚀",
   },
   {
+    id: "8",
     name: "Nessa Mendes",
-    photo: "woman_2",
+    photo: "https://imgur.com/8mJW7v6.png",
     comment: "Agora não preciso mais me preocupar com os boletos",
   },
   {
+    id: "9",
     name: "Enzo Miguel",
-    photo: "man_5",
+    photo: "https://imgur.com/W7qgfsI.png",
     comment:
       "Estava prestes a perder tudo, mas hoje agradeço muito todos os dias por ter encontrado esse método 🙏🏼",
   },
@@ -85,7 +94,7 @@ const insertCommentsOnScreen = () => {
       <figure class="main__comments--section__profile__user--data__figure">
         <img
           class="main__comments--section__profile__user--data__figure__photo"
-          src="./images/${person.photo}.png"
+          src="${person.photo}"
         />
       </figure>
       <p class="main__comments--section__profile__user--data__name">
